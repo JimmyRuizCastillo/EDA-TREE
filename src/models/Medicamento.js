161e2f53c3e7816c6d58a713df0e8d41dc7ea9fb
JimmyRuizCastillo <233288@ids.upchiapas.edu.mx>
2024-07-13 19:50:47 -0600

    Update Tree

--- src/models/Medicamento.js
+++ src/models/Medicamento.js
@@ -2,12 +2,14 @@ class Medicamento{
     name
     price
     amount
+    mg
     id
-    constructor(name, price, amount){
+    constructor(name, price, amount, mg){
         this.id = Math.floor(Math.random() * 9000) + 1000
         this.name = name
         this.price = price
         this.amount = amount
+        this.mg = mg
     }
 }
 
